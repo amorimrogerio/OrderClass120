fc0c7a6a4ca4bb91734fd96b0024b91f818f2dde
Rogerio Amorim <ropamig@gmail.com>
2020-10-01 21:39:11 -0300

    finish Client class and start Start class

--- src/aplication/Start.java
+++ src/aplication/Start.java
@@ -1,10 +1,26 @@
 package aplication;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Start {
 
 	public static void main(String[] args) {
+		Scanner read = new Scanner( System.in );
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy")
+		
+		System.out.println("Enter client data:");
+		System.out.println("------------------");
+		System.out.println("Name: ");String name = read.nextLine();
+		System.out.println("Email: ");String email = read.nextLine();
+		System.out.println("Birth date (DD/MM/YYYY): ");Date birthDate = sdf.parse(read.next());
+		System.out.println("Enter order data:");
+		System.out.println("-----------------");
+		
+		
 
-
+		read.close();
 	}
 
 }
